d86edae1a79c284033340f0f37169e76c783c138
santiago <santiag1725g@gmail.com>
2025-07-07 13:28:05 -0500

    feat: componente Todos.jsx con fetch inicial usando useEffect

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
 function TodoList() {
-  return <div>Listado de tareas</div>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/todos')
+      .then((response) => response.json())
+      .then((data) => setTodos(data))
+      .catch((error) => console.error('Error al obtener los todos:', error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Todos desde API</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
 export default TodoList;
